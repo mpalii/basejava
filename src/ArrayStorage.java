83cbca11bed4820611666c8fa8d3963be6098c8b
mpalii <maksympalii.dev@gmail.com>
2018-06-29 23:09:28 +0300

    Added implementation of methods: clear(), save(Resume r), get(String uuid), delete(String uuid), getAll(), size(), also added private field lastElementIndex.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,55 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private int lastElementIndex = -1;
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
+        lastElementIndex = -1;
     }
 
     void save(Resume r) {
+        if (lastElementIndex < 10000 - 1) {
+            storage[++lastElementIndex] = r;
+        } else {
+            System.out.println("Storage is filled. Your data isn't added.");
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i <= lastElementIndex; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i <= lastElementIndex; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, lastElementIndex - i);
+                lastElementIndex--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        if (lastElementIndex > -1) {
+            return Arrays.copyOfRange(storage, 0, lastElementIndex + 1);
+        } else {
+            return new Resume[0];
+        }
     }
 
     int size() {
-        return 0;
+        return lastElementIndex + 1;
     }
 }
