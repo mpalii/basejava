098a0ac7f338da3d64870dbd92618dc87aa12b2f
mpalii <maksympalii.dev@gmail.com>
2018-08-11 13:20:20 +0300

    Added two utility methods: searchExistentKey(String uuid) and searchNonexistentKey(String uuid).

--- src/ru/javaops/webapp/storage/AbstractStorage.java
+++ src/ru/javaops/webapp/storage/AbstractStorage.java
@@ -6,49 +6,50 @@ import ru.javaops.webapp.model.Resume;
 
 public abstract class AbstractStorage implements Storage {
 
-    protected abstract int executeGetIndex(String uuid);
-    protected abstract void executeUpdate(int index, Resume resume);
-    protected abstract void executeSave(int index, Resume resume);
-    protected abstract Resume executeGet(int index);
-    protected abstract void executeDelete(int index);
+    protected abstract int executeGetKey(String uuid);
+    protected abstract void executeUpdate(int key, Resume resume);
+    protected abstract void executeSave(int key, Resume resume);
+    protected abstract Resume executeGet(int key);
+    protected abstract void executeDelete(int key);
 
     @Override
     public void update(Resume resume) {
-        int index = executeGetIndex(resume.getUuid());
-        if (index > -1) {
-            executeUpdate(index, resume);
-        } else {
-            throw new NotExistStorageException(resume.getUuid());
-        }
+        int key = searchExistentKey(resume.getUuid());
+        executeUpdate(key, resume);
     }
+
 
     @Override
     public void save(Resume resume) {
-        int index = executeGetIndex(resume.getUuid());
-        if (index > -1) {
-            throw new ExistStorageException(resume.getUuid());
-        } else {
-            executeSave(index, resume);
-        }
+        int key = searchNonexistentKey(resume.getUuid());
+        executeSave(key, resume);
     }
 
     @Override
     public Resume get(String uuid) {
-        int index = executeGetIndex(uuid);
-        if (index > -1) {
-            return executeGet(index);
-        } else {
-            throw new NotExistStorageException(uuid);
-        }
+        int key = searchExistentKey(uuid);
+        return executeGet(key);
     }
 
     @Override
     public void delete(String uuid) {
-        int index = executeGetIndex(uuid);
-        if (index > -1) {
-            executeDelete(index);
-        } else {
+        int key = searchExistentKey(uuid);
+        executeDelete(key);
+    }
+
+    private int searchExistentKey(String uuid) {
+        int key = executeGetKey(uuid);
+        if (key < 0) {
             throw new NotExistStorageException(uuid);
         }
+        return key;
+    }
+
+    private int searchNonexistentKey(String uuid) {
+        int key = executeGetKey(uuid);
+        if (key > -1) {
+            throw new ExistStorageException(uuid);
+        }
+        return key;
     }
 }
