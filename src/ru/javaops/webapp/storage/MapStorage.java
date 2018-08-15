876f50d0ac0c46b38f6f19b1ad80f3c84b29a638
mpalii <maksympalii.dev@gmail.com>
2018-08-15 18:28:03 +0300

    Added MapStorage class.

--- src/ru/javaops/webapp/storage/MapStorage.java
+++ src/ru/javaops/webapp/storage/MapStorage.java
@@ -2,48 +2,65 @@ package ru.javaops.webapp.storage;
 
 import ru.javaops.webapp.model.Resume;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapStorage extends AbstractStorage {
+    private Map<String, Resume> storage = new HashMap<>();
 
     /*-----------TO DO-----------*/
 
     @Override
     public void clear() {
-
+        storage.clear();
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resultArr = storage.values().toArray(new Resume[0]);
+        Arrays.sort(resultArr);
+        return resultArr;
     }
 
     @Override
     public int size() {
-        return 0;
+        return storage.size();
     }
 
     @Override
-    protected int executeGetKey(String uuid) {
-        return 0;
+    protected Object executeGetKey(String uuid) {
+        return uuid;
     }
 
     @Override
-    protected void executeUpdate(int index, Resume resume) {
-
+    protected void executeUpdate(Object key, Resume resume) {
+        for (Map.Entry<String, Resume> entry : storage.entrySet()) {
+            if (entry.getKey().equals(key)) {
+                entry.setValue(resume);
+                return;
+            }
+        }
     }
 
     @Override
-    protected void executeSave(int index, Resume resume) {
-
+    protected void executeSave(Object key, Resume resume) {
+        storage.put((String)key, resume);
     }
 
     @Override
-    protected Resume executeGet(int index) {
-        return null;
+    protected Resume executeGet(Object key) {
+        return storage.get(key);
     }
 
     @Override
-    protected void executeDelete(int index) {
+    protected void executeDelete(Object key) {
+        storage.remove(key);
+    }
 
+    @Override
+    protected boolean executeKeyExists(Object key) {
+        return storage.containsKey(key);
     }
 
 
