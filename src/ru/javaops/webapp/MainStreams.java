a3ad15067aad8c5a6e6c8499c9c24d37e4d95906
mpalii <maksympalii.dev@gmail.com>
2018-11-21 21:18:34 +0200

    Streams.

--- src/ru/javaops/webapp/MainStreams.java
+++ src/ru/javaops/webapp/MainStreams.java
@@ -2,48 +2,52 @@ package ru.javaops.webapp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3, 3, 2, 3 };
-        System.out.println(minValue(array));
+        System.out.println(minValue(new int[]{1, 2, 9, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{9, 8}));
 
-        array = new int[]{9, 8};
-        System.out.println(minValue(array));
-
-        List<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(5);
-        list.add(12);
-        list.add(10);
-        list.add(1);
-        list.add(3);
-        list = oddOrEven(list);
-        System.out.println(list);
+        ArrayList<Integer> array = new ArrayList<>();
+        array.add(2);
+        array.add(9);
+        array.add(3);
+        array.add(3);
+        array.add(2);
+        array.add(3);
+        System.out.println(oddOrEven(array));
     }
 
+    /*
+     * реализовать метод через стрим int minValue(int[] values).
+     * Метод принимает массив цифр от 1 до 9, надо выбрать уникальные
+     * и вернуть минимально возможное число, составленное из этих
+     * уникальных цифр. Не использовать преобразование в строку и обратно.
+     * Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
+     */
     private static int minValue(int[] values) {
-        final int[] index = {1};
         return Arrays.stream(values)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
                 .distinct()
-                .map(integer -> {
-                    int result = integer * index[0];
-                    index[0] *= 10;
-                    return result;
-                })
-                .mapToInt(Integer::intValue)
-                .sum();
+                .sorted()
+                .reduce((left, right) -> left * 10 + right)
+                .orElseGet(null);
     }
 
+    /*
+     * реализовать метод List<Integer> oddOrEven(List<Integer> integers)
+     * если сумма всех чисел нечетная - удалить все нечетные, если четная
+     * - удалить все четные. Сложность алгоритма должна быть O(N).
+     * Optional - решение в один стрим.
+     */
     private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
         return integers
                 .stream()
-                .filter(val -> integers.stream().mapToInt(Integer::intValue).sum() % 2 != val % 2)
+                .filter(val -> sum % 2 != val % 2)
                 .collect(Collectors.toList());
     }
+
 }
