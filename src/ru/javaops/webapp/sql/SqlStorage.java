a9eed871c6e4ef7b3a3bd28f3470ae4ed323bf01
mpalii <maksympalii.dev@gmail.com>
2018-11-25 00:16:57 +0200

    SQL storage class and utility classes.

---
+++ src/ru/javaops/webapp/sql/SqlStorage.java
@@ -0,0 +1,82 @@
+package ru.javaops.webapp.sql;
+
+import ru.javaops.webapp.exception.NotExistStorageException;
+import ru.javaops.webapp.exception.StorageException;
+import ru.javaops.webapp.model.Resume;
+import ru.javaops.webapp.storage.Storage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SqlStorage implements Storage {
+
+    public final ConnectionFactory connectionFactory;
+
+    public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
+        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+
+    @Override
+    public void clear() {
+        try (Connection connection = connectionFactory.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM resume")) {
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public void update(Resume resume) {
+        try (Connection connection = connectionFactory.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?, ?)")) {
+            preparedStatement.setString(1, resume.getUuid());
+            preparedStatement.setString(2, resume.getFullName());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        try (Connection connection = connectionFactory.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM resume r WHERE r.uuid = ?")) {
+            preparedStatement.setString(1, uuid);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                throw new NotExistStorageException(uuid);
+            }
+            Resume resume = new Resume(uuid, resultSet.getString("full_name"));
+            return resume;
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
