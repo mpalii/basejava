5a22fbecc20b7f5f93ce34a56d10c962ec36ad2a
mpalii <maksympalii.dev@gmail.com>
2018-12-14 22:24:26 +0200

    HW15 refactoring SqlHelper.

--- src/ru/javaops/webapp/sql/SqlHelper.java
+++ src/ru/javaops/webapp/sql/SqlHelper.java
@@ -13,6 +13,16 @@ public class SqlHelper {
     private final ConnectionFactory connectionFactory;
 
     public SqlHelper(String dbUrl, String dbUser, String dbPassword) {
+//        try {
+//            org.postgresql.Driver.register();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
     }
 
